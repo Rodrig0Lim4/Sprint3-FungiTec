a63b93cd258e8db7aeb0df507b0c750a378baf6b
enzzotj <enzzojaco@gmail.com>
2022-12-03 13:17:15 -0300

    Grafico finalizado

--- site/src/models/medidaModel.js
+++ site/src/models/medidaModel.js
@@ -6,13 +6,13 @@ function buscarUltimasMedidas(fkSensor, limite_linhas) {
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
         instrucaoSql = `select top ${limite_linhas}
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,  
-                        momento,
-                        FORMAT(momento, 'HH:mm:ss') as momento_grafico
-                    from medida
-                    where fk_aquario = ${fkSensor}
-                    order by id desc`;
+        Temperatura as temperatura, 
+        Umidade as umidade,  
+        DiaHora,
+                        FORMAT(DiaHora, 'HH:mm:ss') as momento_grafico
+                    from Dados_Sensor
+                    where fkSensor = ${fkSensor}
+                    order by idDados_Sensor desc`;
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
         instrucaoSql = `select 
         Temperatura as temperatura, 
@@ -36,12 +36,12 @@ function buscarMedidasEmTempoReal(fkSensor) {
 
     if (process.env.AMBIENTE_PROCESSO == "producao") {
         instrucaoSql = `select top 1
-        dht11_temperatura as temperatura, 
-        dht11_umidade as umidade,  
-                        CONVERT(varchar, momento, 108) as momento_grafico, 
-                        fk_aquario 
-                        from medida where fk_aquario = ${fkSensor} 
-                    order by id desc`;
+        Temperatura as temperatura, 
+        Umidade as umidade,  
+                        CONVERT(varchar, DiaHora, 108) as momento_grafico, 
+                        fkSensor 
+                        from Dados_Sensor where fkSensor = ${fkSensor} 
+                    order by idDados_Sensor desc`;
 
     } else if (process.env.AMBIENTE_PROCESSO == "desenvolvimento") {
         instrucaoSql = `select 
